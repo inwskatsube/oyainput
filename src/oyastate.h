93eb5df3e1be5f69d66caaeffb2057766ace148e
inwskatsube <shinkatsube@gmail.com>
2017-05-15 11:09:23 -0700

    update logics

--- src/oyastate.h
+++ src/oyastate.h
@@ -2,14 +2,18 @@
  * oyastate.h
  */
 
-void handle_oyayubi_event(struct OyayubiEvent ev);
 Boolean is_acceptable(int keycode);
 void oyayubi_state_init();
 void create_infotables();
 void close_oya_state();
 void set_left_oyakey(__u16 kc);
 void set_right_oyakey(__u16 kc);
+void set_char_time(long char_time);
+void set_oya_time(long oya_time);
 void set_nicola_time(long nicola_time);
 void add_key_moji(__u16 kc, int m);
 void add_left_key_moji(__u16 kc, int m);
 void add_right_key_moji(__u16 kc, int m);
+long get_current_time();
+void update_event_timer();
+void handle_oyayubi_event(struct OyayubiEvent ev);
